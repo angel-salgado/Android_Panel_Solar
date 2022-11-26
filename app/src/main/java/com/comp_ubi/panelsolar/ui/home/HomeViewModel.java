24916dbe2e04e9fc183d2a9de41eb8ddea06d604
angel-salgado <santiago.salgado@edu.uah.es>
2022-11-26 17:23:43 +0100

    Initial commit

---
+++ app/src/main/java/com/comp_ubi/panelsolar/ui/home/HomeViewModel.java
@@ -0,0 +1,13 @@
+package com.comp_ubi.panelsolar.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+
+    public HomeViewModel() {
+
+    }
+
+}
